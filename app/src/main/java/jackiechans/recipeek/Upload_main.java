a54ab2846f7f4014e6418854940fde0bbc3943ed
JunjieC27 <jackychen27@icloud.com>
2016-11-26 14:22:21 -0500

    Add recepe

---
+++ app/src/main/java/jackiechans/recipeek/Upload_main.java
@@ -0,0 +1,13 @@
+package jackiechans.recipeek;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Upload_main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload_main);
+    }
+}
